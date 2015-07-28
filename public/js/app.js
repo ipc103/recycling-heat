5c27745f59a02634553ba0b801543d64623b1517
Ian Candy <iancandysemail@gmail.com>
2015-07-28 18:35:41 -0400

    stub out heatmap

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+$(function(){
+	getBinData();
+});
+
+var insertMap = function(){
+
+}
+
+var getBinData = function(){
+	var myLatlng = new google.maps.LatLng(40.7127, -74.0059);
+	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+  	center: myLatlng,
+  	zoom: 11,
+  	mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+	var url = "https://data.cityofnewyork.us/resource/sxx4-xhzg.json"
+	var binData = []
+	$.getJSON(url, function(data){
+		$.each(data, function(index, value){
+			if ( !isNaN(parseInt(value.latitude)) ) {
+				binData.push(new google.maps.LatLng(value.latitude, value.longitude));
+			};
+			
+		})
+	var heatmap = new google.maps.visualization.HeatmapLayer({
+  	data: binData
+	});
+	heatmap.setMap(map);
+	})
+	
+}

f31bd7bf58253ecc27254a997ac466e86cf2583f
Ian Candy <iancandysemail@gmail.com>
2015-08-13 22:17:18 -0400

    h1 fade out

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,34 @@
 $(function(){
-	getBinData();
+	$("h1").hide();
+	$("#map-canvas").hide();
+	$("h1").fadeIn(2000).delay(1200).fadeOut('slow');
+	// setTimeout($("#map-canvas").fadeIn, 3000);
+	setTimeout(getBinData, 4000);
 });
 
 var getBinData = function(){
-	var myLatlng = new google.maps.LatLng(40.7127, -74.0059);
+	// creates a new map
+	var myLatlng = new google.maps.LatLng(40.75, -73.95);
 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
   	center: myLatlng,
   	zoom: 11,
   	mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
+
+	// retrieves data from NYC open data
 	var url = "https://data.cityofnewyork.us/resource/sxx4-xhzg.json"
 	var binData = []
 	$.getJSON(url, function(data){
-		$("#total").text("Total Number of Bins: " + data.length.toString());
+		$("#total").text("Total Number of Bins: " + data.length.toString()); // adds total length of the returned bins to the map
+
 		$.each(data, function(index, value){
 			if ( !isNaN(parseInt(value.latitude)) ) {
-				binData.push(new google.maps.LatLng(value.latitude, value.longitude));
+				binData.push(new google.maps.LatLng(value.latitude, value.longitude)); // adds lat and long data if the value is an integer
 			};
 		})
 	var heatmap = new google.maps.visualization.HeatmapLayer({
   	data: binData
 	});
-	heatmap.setMap(map);
+	heatmap.setMap(map); // adds heatmap
 	})
 }
